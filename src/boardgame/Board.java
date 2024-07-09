b111f43dfd4627a41dc81624975a16c38033c4f6
Pedr0f3 <pedrofelipecruz1@gmail.com>
2024-07-09 11:52:44 -0300

    Camada de xadrez e impressão do tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -27,5 +27,11 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	public Piece piece(int row, int colums) {
+		return pieces[row][colums];
+	}
 	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 }
